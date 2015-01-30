94ce94ab8bc33096f615ae55aa5471c085fd0eae
Bojan Mihelac <bmihelac@mihelac.org>
2015-01-30 14:47:04 +0100

    test for 201

--- tests/core/admin.py
+++ tests/core/admin.py
@@ -3,12 +3,22 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 from import_export.admin import ImportExportMixin, ExportActionModelAdmin
+from import_export import resources
+from import_export import fields
 
 from .models import Book, Category, Author
 
 
+class BookResource(resources.ModelResource):
+    name = fields.Field(attribute='name', column_name="Book name")
+
+    class Meta:
+        model = Book
+
+
 class BookAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ['categories', 'author']
+    resource_class = BookResource
 
 
 class CategoryAdmin(ExportActionModelAdmin):
